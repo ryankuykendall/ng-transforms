8c5aff958638e147c979f36eac17034beaa0c8d1
Ryan Kuykendall <ryan.kuykendall@gmail.com>
2019-07-01 08:10:16 -0700

    Adding method for getting Map of decorators by name; light cleanup and reuse of getting name

--- lib/utils/decorator.util.ts
+++ lib/utils/decorator.util.ts
@@ -1,17 +1,28 @@
 import ts from 'typescript';
 
+const getDecoratorName = (decorator: ts.Decorator): string => {
+  const expression = decorator.expression as ts.CallExpression;
+  const identifier = expression.expression as ts.Identifier;
+  return identifier.escapedText as string;
+};
+
 export const getDecoratorNames = (node: ts.Node): Set<string> => {
-  if (node.hasOwnProperty('decorators') && node.decorators) {
-    return new Set<string>(
-      node.decorators.map(decorator => {
-        const expression = decorator.expression as ts.CallExpression;
-        const identifier = expression.expression as ts.Identifier;
-        return identifier.escapedText as string;
-      })
-    );
+  if (Array.isArray(node.decorators)) {
+    return new Set<string>(node.decorators.map(decorator => getDecoratorName(decorator)));
   }
 
   return new Set<string>();
+};
+
+export const getDecoratorMap = (node: ts.Node): Map<string, ts.Decorator> => {
+  if (Array.isArray(node.decorators)) {
+    return node.decorators.reduce((collection, decorator) => {
+      collection.set(getDecoratorName(decorator), decorator);
+      return collection;
+    }, new Map<string, ts.Decorator>());
+  }
+
+  return new Map<string, ts.Decorator>();
 };
 
 export const hasDecoratorWithName = (node: ts.Node, name: string): boolean => {

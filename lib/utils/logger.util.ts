fc72b203d6e028d1cd99e2aa4887047478f7233c
Ryan Kuykendall <ryan.kuykendall@gmail.com>
2019-07-21 12:55:43 -0700

    Fine-tuning logger

--- lib/utils/logger.util.ts
+++ lib/utils/logger.util.ts
@@ -4,23 +4,27 @@ const BOMB: string = '💥';
 const CHECK: string = '✅';
 const HMMM: string = '🤔';
 
-const infoTheme = chalk.green;
-const successTheme = chalk.bgGreen.white;
+const infoTheme = chalk.yellow.bold;
+const successTheme = chalk.bgGreen.black;
 const warnTheme = chalk.bgYellow.black.bold;
 const errorTheme = chalk.bgRed.black.bold;
 
-export const info = (label: string, details: string | undefined) => {
-  console.info(infoTheme(label), details || '');
+export const info = (label: string, ...details: string[]) => {
+  console.info(infoTheme(label), ...details);
 };
 
-export const success = (label: string, details: string | undefined) => {
-  console.log(CHECK, successTheme(label), details || '');
+export const success = (label: string, ...details: string[]) => {
+  console.log(CHECK, successTheme(label), ...details);
 };
 
-export const warn = (label: string, details: string | undefined) => {
-  console.warn(HMMM, warnTheme(label), details || '');
+export const warn = (label: string, ...details: string[]) => {
+  console.warn(HMMM, warnTheme(label), ...details);
 };
 
-export const error = (label: string, details: string | undefined) => {
-  console.error(BOMB, errorTheme(label), details || '');
+export const error = (label: string, ...details: string[]) => {
+  console.error(BOMB, errorTheme(label), ...details);
+};
+
+export const newline = (count: number = 1) => {
+  console.log('\n'.repeat(count));
 };

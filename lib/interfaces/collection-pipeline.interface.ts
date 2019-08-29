2ff4228f119ad483895bbb9fc05454210740c3cc
Ryan Kuykendall <ryan.kuykendall@gmail.com>
2019-08-29 06:19:40 -0700

    Adding more documentation/planning for use of metadata config files

--- lib/interfaces/collection-pipeline.interface.ts
+++ lib/interfaces/collection-pipeline.interface.ts
@@ -5,6 +5,14 @@ export type Filepath = string;
 // QUESTION (ryan): Should developers be able to include/exclude top level
 //   collection types such as ngModules, components, etc.?
 
+// QUESTION (ryan): Should developers be able to include a relative file path
+//   for any of these interfaces (IConfig level, IIncludes level, IExcludes level?)
+
+/**
+ * IConfig resolution priority
+ * 1. Includes builds the collection
+ * 2. Excludes trim down the collection
+ */
 export interface IConfig {
   output: Filepath;
   includes: IIncludes;
@@ -12,12 +20,26 @@ export interface IConfig {
   commands?: ICommands;
 }
 
+/**
+ * IIncludes creates a collection that is a union of:
+ *  - Resolved globs
+ *  - All .ts files in a directory (recursive)
+ *  - All specific .ts files
+ */
 export interface IIncludes {
   globs?: string[];
   directories?: Filepath[];
   files?: Filepath[];
 }
 
+/**
+ * IExcludes prunes the collection created by IInclude
+ * Order of operation:
+ *  1. Prune set based on filepaths collected through resolved globs
+ *  2. Prune set based on .ts filepaths in directory subtree
+ *  3. Prune set based on specific .ts files
+ *  4. Prune set based on tsqueries from ASTs of remaining files
+ */
 export interface IExcludes {
   globs?: string[];
   directories?: Filepath[];

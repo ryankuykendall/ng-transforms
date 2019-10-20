1ffb07d2c6663b220b3fc31b0ff9d654bff0d183
Ryan Kuykendall <ryan.kuykendall@gmail.com>
2019-10-20 12:23:09 -0700

    Running touch over base directories in pipeline

--- lib/commands/collection-pipeline-prime.command.ts
+++ lib/commands/collection-pipeline-prime.command.ts
@@ -1,5 +1,6 @@
 import program from 'commander';
 import process from 'process';
+import { exec } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import logger from './../utils/logger.util';
@@ -25,13 +26,26 @@ export const action = (filepath: string, label: string, cmd: program.Command) =>
   }
 
   const directories: Filepath[] = pipeline.directoriesForPriming();
-  console.info('All primeable directories', directories);
-  return;
 
   directories.forEach(directory => {
     process.chdir(directory);
-    console.info('Changing to pipeline directory', process.cwd());
-    process.chdir('-');
-    console.info('Changing back to initial directory', process.cwd());
+    exec('find ./**/*.ts -exec echo {} \\; -exec touch {} \\;', (error, stdout, stderr) => {
+      if (error !== null) {
+        logger.error('Failed in', directory, error.name, error.message);
+      }
+      const files: Filepath[] = stdout
+        .trim()
+        .split(/\s+/)
+        .filter(file => file.length > 0);
+      if (files.length > 0) {
+        logger.info(
+          `Files in ${directory}: ${files.length} files`,
+          '\n\t - ',
+          files.join('\n\t - ')
+        );
+      } else {
+        logger.warn('No files found in', directory);
+      }
+    });
   });
 };

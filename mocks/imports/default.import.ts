64b6dfba6368a2ba1952375e068a9357ee8f1c40
Ryan Kuykendall <ryan.kuykendall@gmail.com>
2020-04-05 12:00:44 -0700

    Adding comments to mock to note distinction between import types

--- mocks/imports/default.import.ts
+++ mocks/imports/default.import.ts
@@ -1,7 +1,8 @@
-import fs from 'fs';
-import program from 'commander';
-import * as goog from '@angular/compiler';
+import fs from 'fs'; // namespace binding
+import program from 'commander'; // namespace binding
+import * as goog from '@angular/compiler'; // namespace import
 import {
+  // named bindings
   stripQuotes,
   STRIP_QUOTE_CHARS_REGEXP as stripRegexp,
 } from './../../lib/utils/string-literal.util';
